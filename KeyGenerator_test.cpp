35550465e6089d64663efdef68bb0e8864e31466
liuqun <qunliu@zyhx-group.com>
2017-04-07 13:49:03 +0800

    删除重复代码。改为调用子函数中完成加密操作

--- KeyGenerator_test.cpp
+++ KeyGenerator_test.cpp
@@ -53,42 +53,16 @@ int main(int argc, char *argv[])
         }
         printf("}\nkey.length=%d\n", key.length);
     }
-    EVP_CIPHER_CTX context;
     const EVP_CIPHER *Cipher_Blowfish_CBC = EVP_bf_cbc();
     const EVP_CIPHER *cipher = Cipher_Blowfish_CBC;
-    uint8_t ivec[EVP_MAX_IV_LENGTH];
-    memset(ivec, 0x00, EVP_MAX_IV_LENGTH);
-    EVP_EncryptInit(&context, cipher, key.data, ivec);
     const char msg[] = "This is a plain message for test";
-    uint8_t *msg_encrypted;
-    msg_encrypted = new uint8_t[strlen(msg) + key.length];
-    int left; // 剩余待加密字节数
-    left = strlen(msg);
-    int offset;
-    offset = 0;
-    while (left > 0) {
-        int n;
-
-        EVP_EncryptUpdate(&context, msg_encrypted+offset, &n, (uint8_t *)msg+offset, left);
-        offset += n;
-        left -= n;
-    }
-    int tail_len;
-    EVP_EncryptFinal(&context, msg_encrypted+offset, &tail_len);
     if (debug) {
         printf("msg[]=\"%s\"\n", msg);
         printf("strlen(msg)=%d\n", strlen(msg));
-        printf("tail_len=%d\n", tail_len);
-        const int n = strlen(msg) + tail_len;
-        printf("msg_encrypted: 0x%02x,", msg_encrypted[0]);
-        for (int i=1; i<n; i++) {
-            printf("0x%02x,", msg_encrypted[i]);
-        }
-        printf("\n");
     }
-    delete[] msg_encrypted;
-    if (debug) {
-    }
+
+    Debug_MessageEncrypt(cipher, key.data, key.length, (const uint8_t *) msg, strlen(msg));
+
     return (0);
 }
 
